a50057e86b03fc366c41f0ff35a3b74e03e78273
Adriano Proença Barbosa <164345695+AdrianoProenca06@users.noreply.github.com>
2026-04-03 20:49:03 -0300

    Add files via upload
    Atualizacao do codigo completo

---
+++ projeto/api/carros.php
@@ -0,0 +1,79 @@
+<?php
+include '../../config/db.php';
+
+header("Content-Type: application/json");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'), true);
+
+switch ($method) {
+    case 'GET':
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $result = $conn->query("SELECT * FROM veiculo WHERE id=$id");
+            $data = $result->fetch_assoc();
+            echo json_encode($data);
+        }
+        else if (isset($_GET['id_motorista'])) {
+            $id_motorista = $_GET['id_motorista'];
+            $result = $conn->query("SELECT * FROM veiculo WHERE id_motorista=$id_motorista");
+            $carros = [];
+            while ($row = $result->fetch_assoc()) {
+                $carros[] = $row;
+            }
+            echo json_encode($carros);
+            }
+        
+        else {
+            $result = $conn->query("SELECT * FROM veiculo");
+            $users = [];
+            while ($row = $result->fetch_assoc()) {
+                $users[] = $row;
+            }
+            echo json_encode($users);
+        }
+        break;
+
+        
+    case 'POST':
+        $id_motorista = $input['id_motorista'];
+        $modelo = $input['modelo'];
+        $placa = $input['placa'];
+        $n_assentos = $input['n_assentos'];
+        if ($conn->query("INSERT INTO veiculo(id_motorista,modelo,placa,n_assentos) VALUES ('$id_motorista', '$modelo', '$placa', '$n_assentos')")){
+            echo json_encode(["message" => "veiculo ok"]);
+        }
+        else{
+        echo json_encode(["error" => $conn->error]);
+
+        }
+        break;
+
+    case 'PUT':  
+        $id = $_GET['id'] ?? null;      
+        $id_motorista = $input['id_motorista'];
+        $modelo = $input['modelo'];
+        $placa = $input['placa'];
+        $n_assentos = $input['n_assentos'];
+        if ($id) {
+        $conn->query("UPDATE veiculo SET modelo='$modelo', placa='$placa', n_assentos='$n_assentos' WHERE id=$id");
+        echo json_encode(["message" => "veiculo atualizado"]);
+        } 
+        else {
+        echo json_encode(["error" => "veiculo não atualizado"]);
+        }
+        break;
+
+    case 'DELETE':
+        $id = $_GET['id'];
+        $conn->query("DELETE FROM veiculo WHERE id=$id");
+        echo json_encode(["message" => "del sucesso"]);
+        break;
+
+    default:
+        echo json_encode(["message" => "Invalid request method"]);
+        break;
+}
+
+$conn->close();
+?>
---
+++ projeto/db.sql
@@ -0,0 +1,140 @@
+drop database pucaronas;
+CREATE DATABASE pucaronas;
+USE pucaronas;
+CREATE TABLE usuario(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100),
+email VARCHAR(100) UNIQUE,
+senha_hash VARCHAR(128),
+data_nasc DATE,
+status VARCHAR(20),
+cargo ENUM('passageiro','motorista','admin'),
+doc VARCHAR(255)
+);
+
+CREATE TABLE veiculo(
+id INT PRIMARY KEY AUTO_INCREMENT,
+id_motorista INT,
+modelo VARCHAR(128),
+placa VARCHAR(7),
+n_assentos INT,
+FOREIGN KEY (id_motorista) REFERENCES usuario(id)
+);
+ 
+CREATE TABLE carona(
+id INT PRIMARY KEY AUTO_INCREMENT,
+id_motorista INT,
+id_veiculo INT, 
+titulo VARCHAR(50),
+descricao VARCHAR(200),
+mensagem VARCHAR(50),
+vagas INT, 
+status VARCHAR(50), 
+origem VARCHAR(50),
+destino VARCHAR(50),
+FOREIGN KEY (id_motorista) REFERENCES usuario(id),
+FOREIGN KEY (id_veiculo) REFERENCES veiculo(id)
+);
+
+CREATE TABLE aplicacao (
+id INT AUTO_INCREMENT PRIMARY KEY,
+id_passageiro INT,
+id_carona INT,
+status VARCHAR(10), 
+data_aplicacao TIMESTAMP, 
+data_revisao TIMESTAMP,
+mensagem VARCHAR(100),
+FOREIGN KEY (id_passageiro) REFERENCES usuario(id),
+FOREIGN KEY (id_carona) REFERENCES carona(id)
+);
+
+
+CREATE TABLE corrida (
+id INT PRIMARY KEY AUTO_INCREMENT,
+id_motorista INT,
+id_passageiro INT,
+id_carona INT,
+
+data_inicio DATETIME,
+data_fim DATETIME,
+
+origem VARCHAR(100),
+destino VARCHAR(100),
+
+valor DECIMAL(10,2),
+
+status ENUM('pendente','em_andamento','finalizada','cancelada'),
+
+FOREIGN KEY (id_motorista) REFERENCES usuario(id),
+FOREIGN KEY (id_passageiro) REFERENCES usuario(id),
+FOREIGN KEY (id_carona) REFERENCES carona(id)
+);
+
+
+DELIMITER $$
+
+CREATE TRIGGER aplicacao_corrida 
+AFTER UPDATE ON aplicacao
+FOR EACH ROW 
+BEGIN
+    DECLARE v_id_motorista INT;
+    DECLARE v_origem VARCHAR(50);
+    DECLARE v_destino VARCHAR(50);
+    IF NEW.status = 'aprovado' AND OLD.status <> 'aprovado' THEN
+ 
+        SELECT id_motorista, origem, destino 
+        INTO v_id_motorista, v_origem, v_destino
+        FROM carona 
+        WHERE id = NEW.id_carona;
+
+        INSERT INTO corrida (
+            id_motorista, 
+            id_passageiro, 
+            id_carona, 
+            origem, 
+            destino, 
+            status
+        ) VALUES (
+            v_id_motorista,
+            NEW.id_passageiro,
+            NEW.id_carona,
+            v_origem,
+            v_destino,
+            'pendente'
+        );
+        
+        UPDATE carona 
+        SET vagas = vagas - 1 
+        WHERE id = NEW.id_carona AND vagas > 0;
+        
+    END IF;
+END$$
+
+DELIMITER ;
+
+CREATE TABLE log_usuario_status(
+	id_usuario INT,
+	status_anterior VARCHAR(10),
+	status_novo VARCHAR(10),
+	data_hora TIMESTAMP
+);
+
+DELIMITER $$
+
+CREATE TRIGGER usuario_status 
+AFTER UPDATE ON usuario
+FOR EACH ROW 
+BEGIN
+ IF NEW.status = 'aprovado' AND OLD.status <> 'espera' THEN
+INSERT INTO log_usuario_status (id_usuario,status_anterior,status_novo,data_hora) VALUES (OLD.id, OLD.status,NEW.status,NOW() );
+ END IF;
+END $$
+
+DELIMITER ;
+
+
+
+
+
+
+
