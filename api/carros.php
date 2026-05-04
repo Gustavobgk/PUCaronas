4b2dab2927b19c32b6fa2cadedaf925190969941
Adriano <adrianopb2006@gmail.com>
2026-05-04 10:40:40 -0300

    Testando

--- api/carros.php
+++ api/carros.php
@@ -2,7 +2,7 @@
 include '../../config/db.php';
 
 header("Content-Type: application/json");
-
+// s
 $method = $_SERVER['REQUEST_METHOD'];
 $input = json_decode(file_get_contents('php://input'), true);
 
